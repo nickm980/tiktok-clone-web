d0c0c8d1c5fffe2af6d670c0c8ab0905aa44475b
nickm980 <81270095+nickm980@users.noreply.github.com>
2021-11-20 20:32:43 -0500

    Upgraded to SvelteKit

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    console.log("hello")
+</script>
+
+<nav class="navbar">
+    <ul>
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+    </ul>
+</nav>
+
+<style>
+    .navbar {
+        background-color: purple;
+        width: 100%;
+    }
+</style>
